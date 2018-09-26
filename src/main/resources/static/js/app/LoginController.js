ad8c65017afe0c1096168ffaf35b35290b35be18
Boovanahalli Govindegowda <Venkatesha.Boovanahalligovindegowda@CenturyLink.com>
2018-09-26 20:21:16 +0530

    latest changes

--- src/main/resources/static/js/app/LoginController.js
+++ src/main/resources/static/js/app/LoginController.js
@@ -1,12 +1,13 @@
 var app = angular.module('log', []);
 app.controller('LoginController', function($scope) {
-/*	$scope.username = 'venky';*/
-	$scope.password = 'abc';
+/*	$scope.username = 'venky';
+	$scope.password = 'abc';*/
 
 	$scope.login = function() 
 	{
-		if($scope.username =='venky' && $scope.password == 'abc')
+		 if($scope.username =='venky' && $scope.password == 'abc')
 			{
+			 alert("success");
 			console.log("successfully login!!");
 			}
 		else
